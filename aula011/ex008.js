3bcb8ae76aef0296933f5d050da101103b044270
francisco1423mkt <franciscomaitre@outlook.com>
2023-08-16 16:42:34 -0300

    ex010a

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+console.log("o console funcionou corretamente")
+var vel = 60.0
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simple
+    console.log('Você ultrapassou avelocidade permitida. MULTADO!')
+}
+
+
+console.log(`Dirija sempre usando cinto de segurança`)
